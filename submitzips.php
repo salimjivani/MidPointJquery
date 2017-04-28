2bbbf732664588b7a32a81890c15e09cf9d3107d
salimjivani <ssalim.jivani@gmail.com>
2017-04-28 12:51:10 -0500

    Added Index

---
+++ submitzips.php
@@ -0,0 +1,21 @@
+<?php
+
+	foreach ($_POST['zipcodeinput'] as zips)
+	{
+		//insert into db as input key
+	}
+	
+	//serialize duplicates array
+/*
+var activities = ['Location Zero', 'Location One', 'Location Two'];
+$.ajax({        
+       type: "POST",
+       url: "tourFinderFunctions.php",
+       data: { activitiesArray : activities },
+       success: function() {
+            $("#lengthQuestion").fadeOut('slow');        
+       }
+    }); 
+	<? $myArray = $_REQUEST['duplicatezips']; ?>
+*/	
+?>
